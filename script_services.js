21147ce236c9c23b56dc89220e931fc304765596
shadab1928raza <shadab.razamd@gmail.com>
2022-12-12 22:52:59 +0530

    all the pages are updated

--- script_services.js
+++ script_services.js
@@ -1,9 +1,23 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbar = document.getElementsByClassName("navbar")[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-    navbar.classList.toggle('active')
+    navbarLinks.classList.toggle('active')
 });
+
+
+
+// Card Carousel
+// $(".slider").owlCarousel({
+//     loop: true,
+//     autoplay: true,
+//     autoplayTimeout: 2000, //2000ms = 2s;
+//     autoplayHoverPause: true,
+//   });
+
+
+// New Card Carousel
+
 
 
 
